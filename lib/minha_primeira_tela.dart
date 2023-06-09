fd1df515e1b25e275e420ef7bc2156bf11d65011
Gabriel Simon <gabrielsimon775@gmail.com>
2023-06-09 20:33:17 -0300

    First commit

---
+++ lib/minha_primeira_tela.dart
@@ -0,0 +1,39 @@
+// Container(
+// color: Colors.white,
+// child: Column(
+// mainAxisAlignment: MainAxisAlignment.spaceAround,
+// crossAxisAlignment: CrossAxisAlignment.center,
+// children: [
+// Stack(
+// alignment: AlignmentDirectional.center,
+// children: [
+// Container(color: Colors.blue, width: 100, height: 100,),
+// Container(color: Colors.red, width: 50, height: 50,)
+// ],
+// ),
+// Stack(
+// alignment: AlignmentDirectional.center,
+// children: [
+// Container(color: Colors.red, width: 100, height: 100,),
+// Container(color: Colors.blue, width: 50, height: 50,)
+// ],
+// ),
+// Row(
+// mainAxisAlignment: MainAxisAlignment.spaceBetween,
+// crossAxisAlignment: CrossAxisAlignment.center,
+// children: [
+// Container(color: Colors.blue, height: 50, width: 50,),
+// Container(color: Colors.cyan, height: 50, width: 50,),
+// Container(color: Colors.deepOrange, height: 50, width: 50,)
+// ],),
+// Container(
+// color: Colors.amber, height: 30, width: 300,
+// child: Text("opa", style: TextStyle(color: Colors.black, fontSize: 20),
+// textAlign: TextAlign.center),
+// ),
+// ElevatedButton(onPressed: (){
+// print("botao apertado");
+// }, child: Text("botao monstro"))
+// ],
+// ),
+// )
